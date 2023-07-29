5727e148f1c764b545c09af4e6f1c226ef28e061
RicardooFilho <ricardo.tagami17@gmail.com>
2023-07-29 18:44:25 -0300

    Aplicando encapsulamento

--- Admin.js
+++ Admin.js
@@ -1,16 +1,15 @@
 import User from "./User.js";
 
-class Admin extends User {
+export default class Admin extends User {
   constructor (nome, email, nascimento, role = 'admin', ativo = true) {
     super(nome, email, nascimento, role, ativo)
+  }
+
+  nomeAdmin() {
+    return `${this.nome}`
   }
 
   criarCurso (nomeDoCurso, vagas) {
     return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`;
   }
 }
-
-const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01');
-// console.log(novoAdmin);
-// console.log(novoAdmin.exibirInfos());
-console.log(novoAdmin.criarCurso('Java', 20));
